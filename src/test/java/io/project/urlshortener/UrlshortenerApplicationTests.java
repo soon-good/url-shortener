4e478411b82d5541480bd966ce388345ca84fc20
gosgjung <playmymind@daum.net>
2021-03-08 22:17:15 +0900

    프로젝트 초기 구조 커밋

---
+++ src/test/java/io/project/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
